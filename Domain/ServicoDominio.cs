304b23638cad849ae3d49387beda4e5fb625fc1e
washi <washi@DESKTOP-7FVDQ7U>
2021-06-15 18:46:00 -0300

    Add project files.

---
+++ Domain/ServicoDominio.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain
+{
+    public delegate void DelEventHandler(int num);
+
+    public class ServicoDominio
+    {
+        public event DelEventHandler add;
+
+        public void ServicoTrabalhando()
+        {   
+            for (int i = 0; i < 100; i++)
+            {
+                add?.Invoke(i);
+            }
+        }
+    }
+}
---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using System.Windows;
+
+namespace WPF
+{
+    //custom delegate  
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //custom event  
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var servico = new ServicoDominio();
+            servico.add += MainWindow_add;
+            servico.ServicoTrabalhando();
+        }
+
+        private void MainWindow_add(int num)
+        {
+            MessageBox.Show("Olha o número ai " + num);
+        }
+    }
+}
